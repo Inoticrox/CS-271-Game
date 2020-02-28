9bf169c099a0b773e4d1f59cbc16c00e4c7a1e8e
Inoticrox <61395714+Inoticrox@users.noreply.github.com>
2020-02-27 16:15:30 -0800

    commit

--- Game/Game/main.asm
+++ Game/Game/main.asm
@@ -8,13 +8,67 @@ INCLUDELIB C:/Irvine/Irvine32.lib
 .stack 4096
 ExitProcess proto, dwExitCode: dword
 
+mOPrompt MACRO
+	mov EDX, OFFSET oTurn
+	call WriteString
+	call Crlf
+	mov EDX, OFFSET colPrompt
+	call WriteString
+	call ReadInt
+	mov userCol, EAX
+	mov EDX, OFFSET rowPrompt
+	call WriteString
+	call ReadInt
+	mov userRow, EAX
+ENDM
+
+mXPrompt MACRO
+	mov EDX, OFFSET xTurn
+	call WriteString
+	call Crlf
+	mov EDX, OFFSET colPrompt
+	call WriteString
+	call ReadInt
+	mov userCol, EAX
+	mov EDX, OFFSET rowPrompt
+	call WriteString
+	call ReadInt
+	mov userRow, EAX
+ENDM
+
+mWriteStr MACRO input
+	push EDX,
+	mov EDX, OFFSET input
+	call WriteString
+	pop EDX
+ENDM
+
+XTurn
+	mXPrompt
+
+
+XTurn ENDP
+
+OTurn PROC
+	mOPrompt
+
+
+OTurn ENDP
+
+
 .data
 	board DWORD 5 DUP(5 DUP(?))
+	xTurn BYTE "It's X's turn.",0
+	oTurn BYTE "It's O's turn.",0
+	colPrompt BYTE "Enter an empty column number: ",0
+	rowPrompt BYTE "Enter an empty row number: ",0
+	userCol DWORD ?
+	userRow DWORD ?
 
 .code
-
-
 main proc
+	
+
 
 
 	invoke ExitProcess, 0
