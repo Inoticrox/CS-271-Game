345c96078177d447e1b5426630fabb8b3ae2a26b
Inoticrox <61395714+Inoticrox@users.noreply.github.com>
2020-03-03 22:02:10 -0800

    commit

--- Game/Game/main.asm
+++ Game/Game/main.asm
@@ -21,7 +21,7 @@ mOPrompt MACRO
 	mov EDX, OFFSET colPrompt
 	call WriteString
 
-
+ENDM
 
 ;fills rows with inputs 
 mEvenRows MACRO	spaceOne, spaceTwo, spaceThree, row
@@ -310,76 +310,6 @@ mWriteStr MACRO input
 	call WriteString
 	pop EDX
 ENDM
-
-
-
-.data
-	board BYTE 5 DUP(5 DUP(?))
-	xTurn BYTE "It's X's turn.",0
-	oTurn BYTE "It's O's turn.",0
-	colPrompt BYTE "Enter an empty column number: ",0
-	rowPrompt BYTE "Enter an empty row number: ",0
-	colError BYTE "Invalid column entry. Please enter a valid column number: ",0
-	rowError BYTE "Invalid row entry. Please enter a valid row number: ",0
-	userCol DWORD ? 
-	userRow DWORD ? 
-	xChar BYTE "X",0
-	oChar BYTE "O",0
-	works BYTE "Works!",0
-
-.code
-main proc
-	
-	call strtXTurn
-
-colWrong:
-	;Reprompts then checks again
-	mWriteStr colError
-	call Readint
-	jmp colCheck
-
-colRight:
-	mov userCol, EAX
-
-	;Prompts for a row
-	mov EDX, OFFSET rowPrompt
-	call WriteString
-
-	;Sets inputted row to userRow variable
-	call ReadInt
-
-	;Checks for valid row input
-rowCheck:
-	;Checks for non-numbers and negatives
-	cmp EAX, 0
-	jle rowWrong
-
-	;Checks for values larger than number of rows
-	cmp EAX, 3
-	jg rowWrong
-
-	jmp rowRight
-
-rowWrong:
-	;Reprompts then checks again
-	mWriteStr rowError
-	call Readint
-	jmp rowCheck
-
-rowRight:
-	mov userRow, EAX
-
-ENDM
-
-
-mWriteStr MACRO input
-	push EDX
-	mov EDX, OFFSET input
-	call WriteString
-	pop EDX
-ENDM
-
-
 
 .data
 	board		BYTE	5 DUP(5 DUP(?))
