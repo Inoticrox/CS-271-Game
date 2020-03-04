28d87c4b19b4d26bb26e4fb99d9a42b0babb34ca
hughcumber <hughrm23@gmail.com>
2020-03-03 18:54:30 -0800

    got print board working and used a different kind of array

--- Game/Game/main.asm
+++ Game/Game/main.asm
@@ -24,68 +24,85 @@ mWriteStr MACRO input					;	macro to write strings to console
 ENDM
 
 
+mPrint_Ex MACRO
+
+	mWriteStr cols
+
+	mWriteStr rowOne
+	mPrintRow 0
+	call Crlf
+
+	mWriteStr space
+	mPrintRow 1
+	call Crlf
+
+	mWriteStr rowTwo
+	mPrintRow 2
+	call Crlf
+
+	mWriteStr space
+	mPrintRow 3
+	call Crlf
+	
+	mWriteStr rowThree
+	mPrintRow 4
+
+
+ENDM
+
+
+
 
 mEvenRows MACRO	spaceOne, spaceTwo, spaceThree, row						;	fills rows with inputs 
 
+	mov board + (row * 5) , spaceOne
+	mov board + 1 + (row * 5), 124
+	mov board + 2 + (row * 5), spaceTwo
+	mov board + 3 + (row * 5), 124
+	mov board + 4 + (row * 5), spaceThree		
 
-	mov	eax, 4
-	call WriteInt
-	call Crlf
-	mWriteStr spaceOne
-	call WriteInt
-	
+
 ENDM
 
 
+mOddRows MACRO row
 
+	mov board + (row * 5), 95
+	mov board + 1 + (row * 5), 95
+	mov board + 2 + (row * 5), 95
+	mov board + 3 + (row * 5), 95
+	mov board + 4 + (row * 5), 95
 
-mPrint_Helper MACRO	row				;	prints row of parameter 
-
-	
-	mov ECX, 0
-
-	rowloop:
-	mov board[ECX][row], space
-	inc ECX
-	mov board[ECX][row], vert
-
-
-
-	
 
 ENDM
+
+
 
 
 mMake_Board MACRO					;	this should fill the board matrix with the empty board. 
 
 
-	mEvenRows vert, space, 0, 0
-	mEvenRows hor, space, space, 1
-	mEvenRows space, space, space, 2
-	mEvenRows hor, space, space, 3
-	mEvenRows space, space, space, 4
+	mEvenRows 32, 32, 32, 0
+	mOddRows 1
+	mEvenRows 32, 32, 32, 2
+	mOddRows 3
+	mEvenRows 32, 32, 32, 4
+	
 
 
 ENDM
 
+
+
+
 mPrintRow MACRO	row
 
-	mov EDI, 0
-	mov EAX, 5
+	mPrint_indice 0, (row * 5)
+	mPrint_indice 1, (row * 5)
+	mPrint_indice 2, (row * 5)
+	mPrint_indice 3, (row * 5)
+	mPrint_indice 4, (row * 5)
 
-	sloop:
-
-		cmp EDI, 5
-		je exit
-
-		mov mult, board[EDI][row]
-		mWriteStr mult
-		inc EDI
-
-
-	loop sloop
-
-	exit: 
 
 ENDM
 
@@ -95,13 +112,45 @@ mPrint_Board MACRO					;	this should function like for(int i = 0; i < 5; i++){ f
 									
 	
 	mPrintRow 0
+	call Crlf
 	mPrintRow 1
+	call Crlf
 	mPrintRow 2
+	call Crlf
+	mPrintRow 3
+	call Crlf	
 	mPrintRow 4
-	mPrintRow 4
+	call Crlf
+	call Crlf
 
-	
+ENDM
 
+
+mPrint_indice MACRO col, row 
+
+	mov AL, board + col + row
+	call WriteChar
+
+ENDM
+
+
+mPlayer_turn MACRO row, col, X_O			;	a single move, places an X or O
+
+
+	mov EAX, 0
+
+	cmp EAX, X_O
+	je O
+
+
+	X:
+		mov board + ( row * 5 ) + col, 88
+		jmp done
+	O:
+		mov board + ( row * 5 ) + col, 79
+
+
+	done:
 
 ENDM
 
@@ -111,28 +160,25 @@ ENDM
 
 .data
 
-	board	DWORD	5 DUP( 5 DUP(?))
-	tboard	BYTE	25 DUP ("g ")
-	array	BYTE	5 DUP(?)
-	hor		BYTE	"_", 0
-	vert	BYTE	"|", 0
-	space	BYTE	" ", 0
-	i		DWORD	0
-	j		DWORD	0
-	mult	DWORD	? 
-
+	board		BYTE	5 DUP ( 5 DUP (?) )
+	cols		BYTE	"   1 2 3", 10, 0
+	rowOne		BYTE	" 1 ", 0
+	rowTwo		BYTE	" 2 ", 0
+	rowThree	BYTE	" 3 ", 0
+	space		BYTE	"   ", 0
 
 
 .code
 main PROC						;	****************************************
+	
+	
+	mMake_board
 
+	mPrint_Board
 
-	;mov EDX, OFFSET vert
-	;mov board[0][0], EDX
-	;mov EAX, board[0][0]
-	;call mWriteStr EAX
+	mPlayer_turn 0, 0, 0
+	mPrint_Board
 
-	;mov EAX, tboard[0]
 
 
 	exit
